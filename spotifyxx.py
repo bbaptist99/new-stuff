f5723037efb3989a840f7420850dc4b21044ca3b
bbaptist99 <57721157+bbaptist99@users.noreply.github.com>
2020-01-08 21:53:29 -0500

    upload

--- spotifyxx.py
+++ spotifyxx.py
@@ -6,13 +6,15 @@ import webbrowser
 import spotipy.util as util
 from json.decoder import JSONDecodeError
 
+# Get the username
+username = sys.argv[1]
+
 # Authorize the API for the web app
 # export SPOTIPY_CLIENT_ID=''
 # export SPOTIPY_CLIENT_SECRET=''
 # export SPOTIPY_REDIRECT_URI='http://google.com/'
 
-# Get the username
-username = sys.argv[1]
+# run in terminal >> python3 spotifyxx.py bbaptist99
 
  # User ID bbaptist99
  # Erase cache and prompt for user permission
@@ -49,7 +51,7 @@ while True:
 
         # Get search results
         searchResults = spotifyObject.search(searchQuery,1,0,"artist")
-        
+
            # Artist details
         artist = searchResults['artists']['items'][0]
         print(artist['name'])
@@ -57,8 +59,41 @@ while True:
         print(artist['genres'][0])
         print()
         webbrowser.open(artist['images'][0]['url'])
+        artistID = artist['id']
 
+        # Album and track details
+        trackURIs = []
+        trackArt = []
+        z = 0
 
+        #Extract Album data
+        albumResults = spotifyObject.artist_albums(artistID)
+        albumResults = albumResults['items']
+
+        for item in albumResults:
+            print('ALBUM ' + item['name'])
+            albumID = item['id']
+            albumArt = item['images'][0]['url']
+
+            # Extract track data
+            trackResults = spotifyObject.album_tracks(albumID)
+            trackResults = trackResults['items']
+
+            for item in trackResults:
+                print(str(z) + ": " + item['name'])
+                trackURIs.append(item['uri'])
+                trackArt.append(albumArt)
+                z+=1
+            print()
+
+        # See album artist
+        while True:
+            songSelection = input("Enter a song number to see the album art (x to exit): ")
+            if songSelection = "x":
+                break
+            webbrowser.open(trackArt[int(songSelection)])
+
+            
 
     # End the program
     if choice == "1":
